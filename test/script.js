dd2c6b5d1a37363a5b29658496abb3c77df60557
Gerbagel <33951616+Gerbagel@users.noreply.github.com>
2018-10-10 08:38:57 +0200

    Update script.js

--- test/script.js
+++ test/script.js
@@ -1,1 +1,16 @@
-document.write("Ur mum gay");
+var pass = document.getElementById("pass").value;
+		var input = document.getElementById("pass");
+		input.addEventListener("keyup", function(event){
+			event.preventDefault();
+			if (event.keyCode === 13) {
+				document.getElementById("submit").click;
+			}
+		});
+		function login() {
+			if (document.getElementById("pass").value === "gayisok") {
+				window.location.href = "test/";
+			}
+			else {
+				window.location.href = "error/";
+			}
+		}
