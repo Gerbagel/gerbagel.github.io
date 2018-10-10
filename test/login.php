841f8348d20b065f69877369388cc5384ddf6f18
Gerbagel <33951616+Gerbagel@users.noreply.github.com>
2018-10-10 10:18:06 +0200

    jlfh

--- test/login.php
+++ test/login.php
@@ -1,17 +1,13 @@
-<html>
-	<body>
-		<?php
-			$pass = strip_tags($_POST["pass"]);
-			if ($pass == "kevinisgay") {
-				echo '<script type="text/javascript">
-							window.location = "https://gerbagel.github.io/siteisuppose/test/test/"
-					  </script>';
-				die();
-			}
-			else {
-				echo "<h1>Suck my ass senpai</h1>";
-				die();
-			}
-		?>
-	</body>
-</html>
+<?php
+	$pass = strip_tags($_POST["pass"]);
+	if ($pass == "kevinisgay") {
+		echo '<script type="text/javascript">
+					window.location = "https://gerbagel.github.io/siteisuppose/test/test/"
+			  </script>';
+		die();
+	}
+	else {
+		echo "<h1>Suck my ass senpai</h1>";
+		die();
+	}
+?>
