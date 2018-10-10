c09a974a853764e2c8bcec3a0f8ef7cb446c10be
Gerbagel <33951616+Gerbagel@users.noreply.github.com>
2018-10-10 10:01:27 +0200

    idk man

--- test/login.php
+++ test/login.php
@@ -5,6 +5,9 @@
 				echo '<script type="text/javascript">
 							window.location = "https://gerbagel.github.io/siteisuppose/test/test/"
 					  </script>';
+			else
+				echo "<h1>Suck my ass senpai</h1>";
+			
 		?>
 	</body>
 </html>
