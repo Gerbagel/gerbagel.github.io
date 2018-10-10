94de7e847ac7e3112ba2412b6f692e8da965bd47
Gerbagel <33951616+Gerbagel@users.noreply.github.com>
2018-10-10 10:27:35 +0200

    plas

--- test/login.php
+++ test/login.php
@@ -1,13 +1,8 @@
 <?php
 	$pass = strip_tags($_POST["pass"]);
 	if ($pass == "kevinisgay") {
-		echo '<script type="text/javascript">
-					window.location = "https://gerbagel.github.io/siteisuppose/test/test/"
-			  </script>';
-		die();
-	}
-	else {
-		echo "<h1>Suck my ass senpai</h1>";
-		die();
+		header("Location: https://gerbagel.github.io/siteisuppose/test/test/")
+	} else {
+		echo "<h1>Suck my ass senpai</h1>"; die;
 	}
 ?>
