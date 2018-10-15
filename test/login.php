375e40245b9c3c7d7b4826bd935b2e7ffdf50362
Gerbagel <33951616+Gerbagel@users.noreply.github.com>
2018-10-15 12:47:55 +0200

    removal of unnecessary sites

--- test/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$pass = strip_tags($_POST["pass"]);
-	if ($pass == "kevinisgay") {
-		header("Location: https://gerbagel.github.io/siteisuppose/test/test/")
-	} else {
-		echo "<h1>Suck my ass senpai</h1>"; die;
-	}
-?>
